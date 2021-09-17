2385f66869d6e123cfc9f0d52ac7d8d625f2407d
Audun Nes <audun.nes@gmail.com>
2021-09-17 19:06:54 +0200

    Generalize build script

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+FEDORA_VERSION=34
+
 mkdir -p ~/artifacts
 mkdir -p sshkeys
+
+# Remove local artifacts directory if it exists
+if [[ -d "artifacts" ]]; then
+    rm -rf artifacts
+fi
+
 ssh-keygen -q -t rsa -N '' -C 'valyria-linux-key' -f sshkeys/id_rsa
 cp sshkeys/id_rsa ~/.ssh/id_valyria-linux-key
 cp sshkeys/id_rsa.pub ~/.ssh/id_valyria-linux-key.pub
 SSH_RSA_TOKEN=$(cat sshkeys/id_rsa.pub)
-sed -i "s#SSH-RSA-TOKEN#$SSH_RSA_TOKEN#g" http/fedora34-kickstart.cfg
-/usr/bin/packer init -upgrade
-/usr/bin/packer build fedora34.pkr.hcl
-sed -i "s#$SSH_RSA_TOKEN#SSH-RSA-TOKEN#g" http/fedora34-kickstart.cfg
+sed -i "s#SSH-RSA-TOKEN#$SSH_RSA_TOKEN#g" http/fedora${FEDORA_VERSION}-kickstart.cfg
+/usr/bin/packer init -upgrade fedora${FEDORA_VERSION}.pkr.hcl
+/usr/bin/packer build fedora${FEDORA_VERSION}.pkr.hcl
+sed -i "s#$SSH_RSA_TOKEN#SSH-RSA-TOKEN#g" http/fedora${FEDORA_VERSION}-kickstart.cfg
 cp -R artifacts/* ~/artifacts
